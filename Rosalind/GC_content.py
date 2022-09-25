52caa94356d80750decfca20b79cb5506d09c918
Natalia Coutoune <nat.coutoune@gmail.com>
2022-09-25 07:28:56 -0300

    Update GC_content.py
    Add the biopython solution as an alternative

--- Rosalind/GC_content.py
+++ Rosalind/GC_content.py
@@ -47,4 +47,21 @@ MaxGCkay = max(RESULTdict, key = RESULTdict.get)
 print(f'{MaxGCkay[1:]}\n{RESULTdict[MaxGCkay]}')
 
 
+"""
+
+# BioPython solution
   
+from Bio import SeqIO 
+from Bio.SeqUtils import GC
+GCcont = 0
+GCname = ""
+file = open("rosalind_GC.txt", "r")
+for record in SeqIO.parse(file, "fasta"):
+    if GCcont < GC(record.seq):
+        GCcont = GC(record.seq)
+        GCname = record.id
+
+print GCname
+print round(GCcont,2), "%"
+
+"""
